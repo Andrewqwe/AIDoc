44faa5fda18fd8ac2916e5aab320943f11e17d59
Damian Łondka <dlondka@op.pl>
2017-04-06 18:31:53 +0200

    dodano kalendarz i wizyty

--- app/src/main/java/com/damian/aldoc/MainActivity.java
+++ app/src/main/java/com/damian/aldoc/MainActivity.java
@@ -24,15 +24,6 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -83,7 +74,8 @@ public class MainActivity extends AppCompatActivity
 
         if (id == R.id.user_profile) {
         } else if (id == R.id.user_calendar) {
-
+            Intent intent_calendar = new Intent(this, CalendarActivity.class);
+            startActivity(intent_calendar);
         } else if (id == R.id.user_family) {
 
         } else if (id == R.id.user_stats) {
