a4fc732690cee86c2f9092b7c2fcb4b490c56609
mat\Jan Kowalski <218331@student.pwr.edu.pl>
2017-06-11 20:43:31 +0200

    "dokończone" grupy

--- app/src/main/java/com/damian/aldoc/GroupsActivity.java
+++ app/src/main/java/com/damian/aldoc/GroupsActivity.java
@@ -5,31 +5,30 @@ import android.content.DialogInterface;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
-import android.provider.ContactsContract;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.view.*;
+import android.widget.*;
 import com.google.firebase.database.*;
-import org.w3c.dom.Text;
 
 import java.io.FileNotFoundException;
 import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
 public class GroupsActivity extends AppCompatActivity {
 
-    private HashMap<String, View> m_group_views = new HashMap<>();
-    private HashMap<String, View> m_user_views = new HashMap<>();
-    private HashMap<String, ChildEventListener> m_group_listeners = new HashMap<>();
+    /*wiąże view grupy z id grupy której dane view dotyczy*/
+    private HashMap<String, View> m_hmap_groupId_groupView = new HashMap<>();
+    /*wiąże view usera z id usera którego dane view dotyczy*/
+    private HashMap<String, View> m_hmap_userId_userView = new HashMap<>();
+    /*wiąże listenery z id grup których dotyczą*/
+    private HashMap<String, ChildEventListener> m_hmap_groupId_groupListener = new HashMap<>();
+    /*wiąże view usera z id grupy w której dane view się znajduje*/
+    private  HashMap<View, String> m_hmap_userView_groupId = new HashMap<>();
+    /*wiąże view usera z id usera ktorego dane view dotyczy*/
+    private  HashMap<View, String> m_hmap_userView_userId = new HashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,7 +58,7 @@ public class GroupsActivity extends AppCompatActivity {
                         final String user_uid = dataSnapshot.getKey();
 
                         /*znajdujemy view danej grupy w hashmapie*/
-                        final View group_view = m_group_views.get(gid);
+                        final View group_view = m_hmap_groupId_groupView.get(gid);
                         final LayoutInflater inflater = LayoutInflater.from(group_view.getContext());
 
                         /*tworzymy view rzędu przedstawiającego jednego z użytkowników w grupie*/
@@ -108,7 +107,8 @@ public class GroupsActivity extends AppCompatActivity {
                             }
                         });
 
-                        //TODO:Dodać onLongClickiListenera który wyświetli menu z opcjami (np. usuń użytkownika z grupy, zmień uprawnienia itp)
+                        /*dodajemy context menu aby umożliwić różne operacje na użytkownikach w grupie*/
+                        registerForContextMenu(user_row_view);
 
                         /*dodajemy view z rzedem danego uzytkownika do view grupy*/
                         LinearLayout group_layout = (LinearLayout)group_view.findViewById(R.id.userGroup_usersLayout);
@@ -117,7 +117,11 @@ public class GroupsActivity extends AppCompatActivity {
                         /*dodajemy view z rzedem do hashmapy z kluczem uid uzytkownika
                         * zeby potem moc usunac dane view kiedy uzytkownik zostanie usuniety
                         * albo zeby zmodyfikowac view kiedy uzytkownik zostanie zmodyfikowany*/
-                        m_user_views.put(user_uid, user_row_view);
+                        m_hmap_userId_userView.put(user_uid, user_row_view);
+                        /*dodajemy view usera z id grupy oraz z id usera do hashmap
+                         aby rozpoznać użytkownika i grupe po samym view użytkownika*/
+                        m_hmap_userView_groupId.put(user_row_view, gid);
+                        m_hmap_userView_userId.put(user_row_view, user_uid);
                     }
 
                     @Override
@@ -134,12 +138,12 @@ public class GroupsActivity extends AppCompatActivity {
                         final String user_uid = dataSnapshot.getKey();
 
                         /*usuwamy view z rzedem danego uzytkownika z view grupy*/
-                        final View group_view = m_group_views.get(gid);
+                        final View group_view = m_hmap_groupId_groupView.get(gid);
                         LinearLayout group_layout = (LinearLayout)group_view.findViewById(R.id.userGroup_usersLayout);
-                        group_layout.removeView(m_user_views.get(user_uid));
+                        group_layout.removeView(m_hmap_userId_userView.get(user_uid));
 
                         /*usuwamy view z rzedem danego uzytkownika z hashmapy*/
-                        m_user_views.remove(user_uid);
+                        m_hmap_userId_userView.remove(user_uid);
                     }
 
                     @Override
@@ -153,12 +157,12 @@ public class GroupsActivity extends AppCompatActivity {
                 /*dodajemy listenera do hashmapy z kluczem uid grupy
                 * aby pozniej móc usunąć konkretnego listenera od konkretnej grupy
                 * np. kiedy dana grupa zostanie usunieta*/
-                m_group_listeners.put(gid, listener);
+                m_hmap_groupId_groupListener.put(gid, listener);
 
                 /*dla każdej grupy dodajemy View zawierające listę członków danej grupy*/
                 LinearLayout groupsLayout = (LinearLayout)findViewById(R.id.activityGroups_layout);
 
-                /*tworzymy view*/
+                /*tworzymy view grupy*/
                 LayoutInflater inflater = LayoutInflater.from(GroupsActivity.this);
                 LinearLayout group_view = (LinearLayout)inflater.inflate(R.layout.user_group, null);
 
@@ -176,13 +180,23 @@ public class GroupsActivity extends AppCompatActivity {
                     public void onCancelled(DatabaseError databaseError) {}
                 });
 
+                /*znajdujemy w view grupy button służący do dodawania nowych użytkowników
+                * i ustawiamy mu onClickListenera*/
+                final Button btn_addUser = (Button)group_view.findViewById(R.id.userGroup_addUserButton);
+                btn_addUser.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        onAddUserClick(v, gid);
+                    }
+                });
+
                 /*dodajemy view wyświetlające członków grupy do layoutu*/
                 groupsLayout.addView(group_view);
 
                 /*dodajemy stworzone view do hashmapy z kluczem uid grupy
                 * żeby potem móc usunąć konkretne view kiedy zostanie usunięta
                 * konkretna grupa*/
-                m_group_views.put(gid, group_view);
+                m_hmap_groupId_groupView.put(gid, group_view);
             }
 
             @Override
@@ -196,15 +210,15 @@ public class GroupsActivity extends AppCompatActivity {
                 DatabaseReference group_ref = Database.SetLocation(Database.getGroupsDirName()).child(gid);
 
                 /*usuwamy listenera przypisanego do danej grupy (znajdujemy go w hashmapie po kluczu uid grupy)*/
-                group_ref.removeEventListener(m_group_listeners.get(gid));
+                group_ref.removeEventListener(m_hmap_groupId_groupListener.get(gid));
 
                 /*usuwamy z layoutu view odpowiadające danej grupie (view znajdujemy w hashmapie po kluczu uid grupy)*/
                 LinearLayout groupsLayout = (LinearLayout)findViewById(R.id.activityGroups_layout);
-                groupsLayout.removeView(m_group_views.get(gid));
+                groupsLayout.removeView(m_hmap_groupId_groupView.get(gid));
 
                 /*usuwamy view i listenera z hashmap*/
-                m_group_views.remove(gid);
-                m_group_listeners.remove(gid);
+                m_hmap_groupId_groupView.remove(gid);
+                m_hmap_groupId_groupListener.remove(gid);
             }
 
             @Override
@@ -243,4 +257,111 @@ public class GroupsActivity extends AppCompatActivity {
         Dialog createGroupDialog = createGroupDialogBuilder.create();
         createGroupDialog.show();
     }
+
+    private View contextView;
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo)
+    {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        getMenuInflater().inflate(R.menu.menu_group_user, menu);
+        contextView = v;
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item)
+    {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        final View user_view = contextView;
+
+        switch (item.getItemId())
+        {
+            case R.id.userGroupMenu_delete:
+                AlertDialog.Builder alert_delete = new AlertDialog.Builder(this);
+                alert_delete.setTitle("Czy na pewno chcesz usunąć użytkownika z grupy?");
+
+                alert_delete.setCancelable(true).setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which)
+                    {
+                        String gid = m_hmap_userView_groupId.get(user_view);
+                        String uid = m_hmap_userView_userId.get(user_view);
+                        Database.removeUserFromGroup(uid, gid);
+                    }
+                });
+
+                alert_delete.setNegativeButton("Anuluj", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                });
+
+                Dialog delete_dialog = alert_delete.create();
+                delete_dialog.show();
+
+                break;
+            default:
+                break;
+        }
+
+        return super.onContextItemSelected(item);
+    }
+
+    private void onAddUserClick(View view, final String group_id)
+    {
+        AlertDialog.Builder alert_add = new AlertDialog.Builder(this);
+        alert_add.setTitle("Dodaj użytkownika");
+
+        View dialog_view = (LayoutInflater.from(this)).inflate(R.layout.group_add_user, null);
+        alert_add.setView(dialog_view);
+
+        final EditText email_et = (EditText)dialog_view.findViewById(R.id.groupAddUser_email);
+
+        alert_add.setCancelable(true).setPositiveButton("Dodaj", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which)
+            {
+                /*czytamy z pola tekstowego email podany przez użytkownika*/
+                final String email = email_et.getText().toString();
+                /*wyszukujemy użytkownika o podanym emailu i jeżeli istnieje to dodajemy go do grupy*/
+                Query q = Database.SetLocation(Database.getUsersDirName()).orderByChild("email").equalTo(email);
+                q.addChildEventListener(new ChildEventListener() {
+                    @Override
+                    public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                        Database.addUserToGroup(dataSnapshot.getKey(), group_id, Long.valueOf(1));
+                    }
+
+                    @Override
+                    public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+                    }
+
+                    @Override
+                    public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+                    }
+
+                    @Override
+                    public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+            }
+        });
+
+        alert_add.setNegativeButton("Anuluj", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+            }
+        });
+
+        Dialog add_dialog = alert_add.create();
+        add_dialog.show();
+    }
 }
+
